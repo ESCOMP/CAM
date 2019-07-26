05f1668bdc34943924918b506947a67d79ce54f0
Brian Eaton <eaton@ucar.edu>
2019-07-26 14:05:07 -0600

    'FV dycore_is bug fix
    Imported from https://svn-ccsm-models.cgd.ucar.edu/cam1/branches/cam_cesm2_1_rel@91447
    Committed by Brian Eaton <eaton@ucar.edu> at 2019-07-26 14:05:07 -0600
    Original svn commit message:
    
    FV dycore_is bug fix'

--- src/dynamics/fv/dycore.F90
+++ src/dynamics/fv/dycore.F90
@@ -1,5 +1,7 @@
 module dycore
 
+use string_utils, only: to_upper
+
 implicit none
 private
 save
@@ -14,10 +16,14 @@ logical function dycore_is (name)
 
    ! Determine the dynamical core in use.
 
-   character(len=*) :: name
+   character(len=*), intent(in) :: name
+
+   character(len=len(name)) :: uname
    !-----------------------------------------------------------------------
 
-   if (name == 'lr' .or. name == 'LR') then
+   uname = to_upper(name)
+
+   if (uname == 'LR' .or. uname == 'FV') then
       dycore_is = .true.
    else
       dycore_is = .false.
