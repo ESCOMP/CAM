812d01e9d5976229d660b16a8649ea9af3899900
Cheryl Craig <cacraig@ucar.edu>
2020-01-10 11:05:16 -0700

    Add missing continuation

--- src/physics/cam/cam_diagnostics.F90
+++ src/physics/cam/cam_diagnostics.F90
@@ -1410,7 +1410,7 @@ contains
       call vertinterp(ncol, pcols, pver, state%pmid, 100000._r8, state%q(1,1,1), p_surf_q1)
     end if
 
-    if (hist_fld_active('THE9251000') .or. 
+    if (hist_fld_active('THE9251000') .or.  &
         hist_fld_active('Q925')) then
       call vertinterp(ncol, pcols, pver, state%pmid, 92500._r8, state%q(1,1,1), p_surf_q2)
     end if
