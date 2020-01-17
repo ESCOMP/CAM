db6a9359039dd452f96975c380d0f7b32a8cfab7
Michael Duda <duda@ucar.edu>
2020-01-17 12:12:38 -0700

    Add new routine cam_mpas_cell_to_edge_winds to cam_mpas_subdriver
    This commit adds a new routine to the cam_mpas_subdriver module to be used in
    setting up initial conditions for horizontal velocity and in transforming
    horizontal velocity tendencies from physics to dynamics. The routine projects
    cell-centered wind vectors onto the unit normal vectors at edges. In support of
    the use of this routine, several new variables have been added to the dynamics
    import and export states.

--- src/dynamics/mpas/driver/cam_mpas_subdriver.F90
+++ src/dynamics/mpas/driver/cam_mpas_subdriver.F90
@@ -23,6 +23,7 @@ module cam_mpas_subdriver
               cam_mpas_read_static, &
               cam_mpas_compute_unit_vectors, &
               cam_mpas_update_halo, &
+              cam_mpas_cell_to_edge_winds, &
               cam_mpas_finalize
     public :: corelist, domain_ptr
 
@@ -1110,6 +1111,62 @@ contains
        end if
 
     end subroutine cam_mpas_update_halo
+
+
+    !-----------------------------------------------------------------------
+    !  routine cam_mpas_cell_to_edge_winds
+    !
+    !> \brief  Projects cell-centered winds to the normal component of velocity on edges
+    !> \author Michael Duda
+    !> \date   16 January 2020
+    !> \details
+    !>  Given zonal and meridional winds at cell centers, unit vectors in the east
+    !>  and north directions at cell centers, and unit vectors in the normal
+    !>  direction at edges, this routine projects the cell-centered winds onto
+    !>  the normal vectors.
+    !>
+    !>  Prior to calling this routine, the halos for the zonal and meridional
+    !>  components of cell-centered winds should be updated. It is also critical
+    !>  that the east, north, uZonal, and uMerid field are all allocated with
+    !>  a "garbage" element; this is handled automatically for fields allocated
+    !>  by the MPAS infrastructure.
+    !
+    !-----------------------------------------------------------------------
+    subroutine cam_mpas_cell_to_edge_winds(nEdges, uZonal, uMerid, east, north, edgeNormalVectors, &
+                                           cellsOnEdge, uNormal)
+
+       use mpas_kind_types, only : RKIND
+
+       implicit none
+
+       integer, intent(in) :: nEdges
+       real(kind=RKIND), dimension(:,:), intent(in) :: uZonal, uMerid
+       real(kind=RKIND), dimension(:,:), intent(in) :: east, north, edgeNormalVectors
+       integer, dimension(:,:), intent(in) :: cellsOnEdge
+       real(kind=RKIND), dimension(:,:), intent(out) :: uNormal
+
+       integer :: iEdge, cell1, cell2
+
+
+       do iEdge = 1, nEdges
+          cell1 = cellsOnEdge(1,iEdge)
+          cell2 = cellsOnEdge(2,iEdge)
+
+          uNormal(:,iEdge) =  uZonal(:,cell1) * 0.5 * (edgeNormalVectors(1,iEdge) * east(1,cell1)   &
+                                                    +  edgeNormalVectors(2,iEdge) * east(2,cell1)   &
+                                                    +  edgeNormalVectors(3,iEdge) * east(3,cell1))  &
+                            + uMerid(:,cell1) * 0.5 * (edgeNormalVectors(1,iEdge) * north(1,cell1)   &
+                                                    +  edgeNormalVectors(2,iEdge) * north(2,cell1)   &
+                                                    +  edgeNormalVectors(3,iEdge) * north(3,cell1))  &
+                            + uZonal(:,cell2) * 0.5 * (edgeNormalVectors(1,iEdge) * east(1,cell2)   &
+                                                    +  edgeNormalVectors(2,iEdge) * east(2,cell2)   &
+                                                    +  edgeNormalVectors(3,iEdge) * east(3,cell2))  &
+                            + uMerid(:,cell2) * 0.5 * (edgeNormalVectors(1,iEdge) * north(1,cell2)   &
+                                                    +  edgeNormalVectors(2,iEdge) * north(2,cell2)   &
+                                                    +  edgeNormalVectors(3,iEdge) * north(3,cell2))
+       end do
+
+    end subroutine cam_mpas_cell_to_edge_winds
 
 
     !-----------------------------------------------------------------------
