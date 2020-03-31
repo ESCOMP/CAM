55048d3450d3e85087b8f304fbddd35c58858cb5
Chris A. Fischer <fischer@izumi.unified.ucar.edu>
2020-03-31 15:06:41 -0600

    Remove extra files

--- cime_config/testdefs/testmods_dirs/cam/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-listing=`ls`
-#echo ${listing}
-listings=( $listing )
-for dir in "${listings[@]}"
-do
-   echo ./xmlchange CAM_NML_USE_CASE="UNSET" >> $dir/shell_commands
-done
--- cime_config/testdefs/testmods_dirs/cam/job2.sh
+++
@@ -1,9 +0,0 @@
-rm cam4_port/shell_commands
-rm cam5_port_f45/shell_commands
-rm cam5_port_ne30/shell_commands
-rm cam6_port_f09/shell_commands
-rm fire_emis/shell_commands
-rm outfrq1m/shell_commands
-rm reduced_hist1d/shell_commands
-rm reduced_hist3s/shell_commands
-rm reduced_hist5d/shell_commands
