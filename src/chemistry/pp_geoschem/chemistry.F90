d702e463644a45acdcaee7daabfbcddf013c159d
Thibaud Fritz <fritzt@mit.edu>
2020-05-14 16:11:27 -0400

    Feat: Initial implementation of compilable code
    (1) The code can now be compiled using a CAM 4.0 + "dummy" GEOS-Chem
        component set (compset FGC). This is based on the "tropospheric
        mozart" set.
    
    Reproducing commit from SDE - May 17, 2018

--- src/chemistry/pp_geoschem/chemistry.F90
+++ src/chemistry/pp_geoschem/chemistry.F90
@@ -8,12 +8,14 @@ module chemistry
   use ppgrid,              only: begchunk, endchunk, pcols
   use ppgrid,              only: pver
   use constituents,        only: pcnst, cnst_add
-  use mo_gas_phase_chemdr, only: map2chm
-  use mo_constants,        only: pi
+  !use mo_gas_phase_chemdr, only: map2chm
+  !use mo_constants,        only: pi
   use shr_const_mod,       only: molw_dryair=>SHR_CONST_MWDAIR
-  use mo_chem_utls,        only : get_spc_ndx
-  use chem_mods,           only : gas_pcnst, adv_mass
-  use mo_sim_dat, only: set_sim_dat
+  !use mo_chem_utls,        only : get_spc_ndx
+  !use chem_mods,           only : gas_pcnst, adv_mass
+  !use mo_sim_dat, only: set_sim_dat
+  use spmd_utils,          only : masterproc
+  use cam_logfile,         only : iulog
   implicit none
   private
   save
@@ -32,18 +34,23 @@ module chemistry
   public :: chem_read_restart
   public :: chem_init_restart
   public :: chem_readnl                    ! read chem namelist 
-  public :: chem_reset_fluxes
 
-  interface chem_write_restart
-     module procedure chem_write_restart_bin
-     module procedure chem_write_restart_pio
-  end interface
-  interface chem_read_restart
-     module procedure chem_read_restart_bin
-     module procedure chem_read_restart_pio
-  end interface
+  public :: chem_emissions
+  public :: chem_timestep_init
 
   ! Private data
+  !===== SDE DEBUG =====
+  integer, parameter :: ntracersmax = 200    ! Must be equal to nadv_chem
+  integer            :: ntracers
+  character(len=255) :: tracernames(ntracersmax)
+  integer            :: indices(ntracersmax)
+  real(r8)           :: adv_mass(ntracersmax)
+
+  ! Short-lived species (i.e. not advected)
+  integer, parameter :: nslsmax = 500        ! UNadvected species only
+  integer            :: nsls
+  character(len=255) :: slsnames(nslsmax)
+  !===== SDE DEBUG =====
 
 !================================================================================================
 contains
@@ -57,17 +64,95 @@ contains
     if (name == 'geoschem' ) then
        chem_is = .true.
     end if
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_IS'
 
   end function chem_is
 
 !================================================================================================
 
   subroutine chem_register
+
+    use physics_buffer, only : pbuf_add_field, dtype_r8
     !----------------------------------------------------------------------- 
     ! 
-    ! Purpose: register advected constituents for parameterized greenhouse gas chemistry
+    ! Purpose: register advected constituents for chemistry
     ! 
     !-----------------------------------------------------------------------
+    integer            :: i, n
+    real(r8)           :: cptmp
+    real(r8)           :: qmin
+    character(len=128) :: mixtype
+    character(len=128) :: molectype
+    character(len=128) :: lng_name
+    logical            :: camout
+    logical            :: ic_from_cam2
+    logical            :: has_fixed_ubc
+    logical            :: has_fixed_ubflx
+    ! SDE 2018-05-02: This seems to get called before anything else
+    ! That includes CHEM_INIT
+    ! At this point, mozart calls SET_SIM_DAT, which is specified by each
+    ! mechanism separately (ie mozart/chemistry.F90 calls the subroutine
+    ! set_sim_dat which is in pp_[mechanism]/mo_sim_dat.F90. That sets a lot of
+    ! data in other places, notably in "chem_mods"
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_REGISTER'
+    ! At the moment, we force nadv_chem=200 in the setup file
+    ntracers = 200
+    do i = 1, ntracersmax
+       ! TODO: Read input.geos in chem_readnl to get tracernames(1:ntracers)
+       ! TODO: Get all other species properties here from species database
+       ! Hard-code for now
+       select case (tracernames(i))
+          case ('BCPI')
+             lng_name = 'Hydrophilic black carbon'
+             ! Molar mass (g/mol)
+             adv_mass(i) = 1000.0e+0_r8 * (0.012e+0_r8)
+          case ('OCS')
+             lng_name = 'Carbonyl sulfide'
+             ! Molar mass (g/mol)
+             adv_mass(i) = 1000.0e+0_r8 * (0.060e+0_r8)
+          case default
+             lng_name = tracernames(i)
+             adv_mass(i) = 1000.0e+0_r8 * (0.001e+0_r8)
+       end select
+       ! dummy value for specific heat of constant pressure (Cp)
+       cptmp = 666._r8
+       ! minimum mixing ratio
+       qmin = 1.e-36_r8
+       ! mixing ratio type
+       mixtype = 'dry'
+       ! Used for ionospheric WACCM (WACCM-X)
+       molectype = 'minor'
+       ! Is an output field (?)
+       camout = .false.
+       ! Not true for O2(1-delta) or O2(1-sigma)
+       ic_from_cam2  = .true.
+       ! Use a fixed value at the upper boundary
+       has_fixed_ubc = .false.
+       ! Use a fixed flux condition at the upper boundary
+       has_fixed_ubflx = .false.
+       !write(tracernames(i),'(a,I0.4)') 'GCTRC_', i
+       ! NOTE: In MOZART, this only gets called for tracers
+       ! This is the call to add a "constituent"
+       call cnst_add( trim(tracernames(i)), adv_mass(i), cptmp, qmin, n, &
+                      readiv=ic_from_cam2, mixtype=mixtype, cam_outfld=camout, &
+                      molectype=molectype, fixed_ubc=has_fixed_ubc, &
+                      fixed_ubflx=has_fixed_ubflx, longname=trim(lng_name) )
+       ! MOZART uses this for short-lived species. Not certain exactly what it
+       ! does, but note that the "ShortLivedSpecies" physics buffer already
+       ! needs to have been initialized, which we haven't done. Physics buffers
+       ! are fields which are available either across timesteps or for use to
+       ! modules outside of chemistry
+       ! More information:
+       ! http://www.cesm.ucar.edu/models/atm-cam/docs/phys-interface/node5.html
+       !call pbuf_add_field('ShortLivedSpecies','global',dtype_r8,(/pcols,pver,nslvd/),pbf_idx)
+       ! returned values
+       !  n : mapping in CAM
+       ! map2chm is a mozart variable
+       !map2chm(n) = i
+       !indices(i) = 0
+       ! ===== SDE DEBUG =====
+    end do
 
   end subroutine chem_register
 
@@ -76,6 +161,30 @@ contains
 
     ! args
     character(len=*), intent(in) :: nlfile  ! filepath for file containing namelist input
+
+    ! Local variables
+    integer :: i
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_READNL'
+
+    ! TODO: Read in input.geos and get species names
+    ntracers=2
+    do i=1,ntracers
+       ! TEMPORARY: Hardcode 2 species (BCPI in output, H2 still needed for
+       ! lower boundary condition)
+       if (i==1) then
+          tracernames(i) = 'BCPI'
+       elseif (i==2) then
+          tracernames(i) = 'OCS'
+       else
+          write(tracernames(i),'(a,I0.4)') 'GCTRC_',i
+       end if
+    end do
+
+    ! Assign remaining species dummy names
+    do i=(ntracers+1),ntracersmax
+       write(tracernames(i),'(a,I0.4)') 'GCTRC_',i
+    end do
 
   end subroutine chem_readnl
 
@@ -86,6 +195,7 @@ contains
     logical :: chem_is_active
     !-----------------------------------------------------------------------
     chem_is_active = .true.
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_IS_ACTIVE'
   end function chem_is_active
 
 !================================================================================================
@@ -108,13 +218,14 @@ contains
     
     chem_implements_cnst = .false.
 
-    do i = 1, nspecies
-       if (trim(species(i)) .eq. trim(name)) then
+    do i = 1, ntracersmax
+       if (trim(tracernames(i)) .eq. trim(name)) then
           chem_implements_cnst = .true.
           exit
        end if
     end do
 
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_IMPLEMENTS_CNST'
   end function chem_implements_cnst
 
 !===============================================================================
@@ -126,16 +237,64 @@ contains
     !          (and declare history variables)
     ! 
     !-----------------------------------------------------------------------
+    use physics_buffer, only: physics_buffer_desc
+    use cam_history,    only: addfld, add_default, horiz_only
+
+    type(physics_state), intent(in):: phys_state(begchunk:endchunk)
+    type(physics_buffer_desc), pointer :: pbuf2d(:,:)
 
     ! Can add history output here too with the "addfld" & "add_default" routines
+    call addfld ( 'BCPI', (/'lev'/), 'A', 'mole/mole', trim('BCPI')//' mixing ratio' )
+    call add_default ( 'BCPI',   1, ' ')
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_INIT'
 
   end subroutine chem_init
 
 !===============================================================================
 
+  subroutine chem_timestep_init(phys_state, pbuf2d)
+    use physics_buffer,   only: physics_buffer_desc
+
+    type(physics_state), intent(in):: phys_state(begchunk:endchunk)
+    type(physics_buffer_desc), pointer :: pbuf2d(:,:)
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_TIMESTEP_INIT'
+  end subroutine chem_timestep_init
+
+!===============================================================================
+
   subroutine chem_timestep_tend( state, ptend, cam_in, cam_out, dt, pbuf,  fh2o )
 
+    use physics_buffer,   only: physics_buffer_desc
+    use cam_history,      only: outfld
+    use camsrfexch,       only: cam_in_t, cam_out_t
+
+    real(r8),            intent(in)    :: dt          ! time step
+    type(physics_state), intent(in)    :: state       ! Physics state variables
+    type(physics_ptend), intent(out)   :: ptend       ! indivdual parameterization tendencies
+    type(cam_in_t),      intent(inout) :: cam_in
+    type(cam_out_t),     intent(in)    :: cam_out
+    type(physics_buffer_desc), pointer :: pbuf(:)
+    real(r8), optional,  intent(out)   :: fh2o(pcols) ! h2o flux to balance source from chemistry
+
+    ! Mapping (?)
+    logical :: lq(pcnst)
+    integer :: n, m
     ! Here's where you'll call DO_CHEMISTRY
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_TIMESTEP_TEND'
+    lq(:) = .false.
+    do n=1,pcnst
+        !m = map2chm(n)
+        m=0
+        if (m > 0) lq(n) = .true.
+    end do
+    call physics_ptend_init(ptend, state%psetcols, 'chemistry', lq=lq)
+    ! ptend%q dimensions: [column, ?, species]
+    !ptend%q(:ncol,:,:) = 0.0e+0_r8
+    !ptend%q(:ncol,:,:) = 0.0e+0_r8
+    ptend%q(:,:,:) = 0.0e+0_r8
+    if (present(fh2o)) fh2o(:) = 0.0e+0_r8
 
     return
   end subroutine chem_timestep_tend
@@ -143,9 +302,28 @@ contains
 !===============================================================================
   subroutine chem_init_cnst(name, latvals, lonvals, mask, q)
 
+    character(len=*), intent(in)  :: name       !  constituent name
+    real(r8),         intent(in)  :: latvals(:) ! lat in degrees (ncol)
+    real(r8),         intent(in)  :: lonvals(:) ! lon in degrees (ncol)
+    logical,          intent(in)  :: mask(:)    ! Only initialize where .true.
+    real(r8),         intent(out) :: q(:,:)     ! kg tracer/kg dry air (ncol, plev
     ! Used to initialize tracer fields if desired.
     ! Will need a simple mapping structure as well as the CAM tracer registration
     ! routines.
+
+    integer :: ilev, nlev
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_INIT_CNST'
+
+    nlev = size(q, 2)
+    if ( any( tracernames .eq. name ) ) then
+       do ilev=1,nlev
+          where(mask)
+             ! Set to the minimum mixing ratio
+             q(:,ilev) = 1.0e-38_r8
+          end where
+       end do
+    end if
 
   end subroutine chem_init_cnst
 
@@ -153,46 +331,62 @@ contains
   subroutine chem_final
     
     ! Finalize GEOS-Chem
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_FINAL'
 
     return
   end subroutine chem_final
 !===============================================================================
-  subroutine chem_write_restart_bin( nrg )
-    implicit none
-    integer,intent(in) :: nrg     ! Unit number
-    return
-  end subroutine chem_write_restart_bin
-!===============================================================================
-  subroutine chem_read_restart_bin( nrg )
-    implicit none
-    integer,intent(in) :: nrg     ! Unit number
-    return
-  end subroutine chem_read_restart_bin
-!===============================================================================
-  subroutine chem_write_restart_pio( File )
-    use pio, only : file_desc_t
-    type(file_desc_t) :: File
-    return
-  end subroutine chem_write_restart_pio
-!===============================================================================
-  subroutine chem_read_restart_pio( File )
-    use pio, only : file_desc_t
-    type(file_desc_t) :: File
-    return
-  end subroutine chem_read_restart_pio
-!===============================================================================
   subroutine chem_init_restart(File)
     use pio, only : file_desc_t
     type(file_desc_t) :: File
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_INIT_RESTART'
     return
   end subroutine chem_init_restart
+!===============================================================================
+  subroutine chem_write_restart( File )
+    !use tracer_cnst, only: write_tracer_cnst_restart
+    !use tracer_srcs, only: write_tracer_srcs_restart
+    !use linoz_data,  only: write_linoz_data_restart
+    use pio, only : file_desc_t
+    implicit none
+    type(file_desc_t) :: File
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_WRITE_RESTART'
+    !
+    ! data for offline tracers
+    !
+    !call write_tracer_cnst_restart(File)
+    !call write_tracer_srcs_restart(File)
+    !call write_linoz_data_restart(File)
+  end subroutine chem_write_restart
+!===============================================================================
+  subroutine chem_read_restart( File )
+    !use tracer_cnst, only: read_tracer_cnst_restart
+    !use tracer_srcs, only: read_tracer_srcs_restart
+    !use linoz_data,  only: read_linoz_data_restart
+
+    use pio, only : file_desc_t
+    implicit none
+    type(file_desc_t) :: File
+
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_READ_RESTART'
+    !
+    ! data for offline tracers
+    !
+    !call read_tracer_cnst_restart(File)
+    !call read_tracer_srcs_restart(File)
+    !call read_linoz_data_restart(File)
+  end subroutine chem_read_restart
 !================================================================================
-  subroutine chem_reset_fluxes( fptr, cam_in )
+  subroutine chem_emissions( state, cam_in )
     use camsrfexch, only : cam_in_t     
 
-    real(r8), pointer             :: fptr(:,:)        ! pointer into    array data
-    type(cam_in_t), intent(inout) :: cam_in(begchunk:endchunk)
+    ! Arguments:
 
-  end subroutine chem_reset_fluxes
+    type(physics_state),    intent(in)    :: state   ! Physics state variables
+    type(cam_in_t),         intent(inout) :: cam_in  ! import state
+    if (masterproc) write(iulog,'(a)') 'GCCALL CHEM_EMISSIONS'
+
+  end subroutine chem_emissions
 
 end module chemistry
