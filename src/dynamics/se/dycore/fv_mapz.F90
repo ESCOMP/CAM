2e8652c95469343dba36149fbcfa5a7a29218362
PeterHjortLauritzen <pel@ucar.edu>
2020-06-01 15:56:18 -0600

    text for FV3 code; this version sent to Eaton for CESM2.2

--- src/dynamics/se/dycore/fv_mapz.F90
+++ src/dynamics/se/dycore/fv_mapz.F90
@@ -1,8 +1,29 @@
+
+!***********************************************************************
+!*                   GNU Lesser General Public License                 
+!*
+!* This file is part of the FV3 dynamical core.
+!*
+!* The FV3 dynamical core is free software: you can redistribute it 
+!* and/or modify it under the terms of the
+!* GNU Lesser General Public License as published by the
+!* Free Software Foundation, either version 3 of the License, or 
+!* (at your option) any later version.
+!*
+!* The FV3 dynamical core is distributed in the hope that it will be 
+!* useful, but WITHOUT ANYWARRANTY; without even the implied warranty 
+!* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+!* See the GNU General Public License for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with the FV3 dynamical core.  
+!* If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
 module fv_mapz
   
   !**************************************************************************************
   !
-  ! Vertical remapping algorithms from FV3
+  ! Vertical remapping algorithms from FV3 minimally modified for use in CAM
   !
   !**************************************************************************************
   
