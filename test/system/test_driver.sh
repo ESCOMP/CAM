cf43c94ecca82e90ad030a36f38c98517b70b48b
Cheryl Craig <cacraig@ucar.edu>
2020-12-01 15:20:58 -0700

    final updates including ChangeLog text - no testing reported yet:q

--- test/system/test_driver.sh
+++ test/system/test_driver.sh
@@ -324,8 +324,11 @@ EOF
     izu* | i[[:digit:]]* )
     
     # Run git and r8 tests
+    echo "###################################################"
     export ADDREALKIND_EXE=/fs/cgd/csm/tools/addrealkind/addrealkind;  ${CAM_ROOT}/test/system/TR8.sh
+    echo "###################################################"
     ${CAM_ROOT}/test/system/TGIT.sh
+    echo "###################################################"
 
     submit_script_cime="`pwd -P`/test_driver_izumi_cime_${cur_time}.sh"
     export PATH=/cluster/torque/bin:${PATH}
