d924b90189871cfe60bbd34b557407f982a5f4b1
Thibaud Fritz <fritzt@mit.edu>
2021-03-23 13:27:52 -0400

    Feat: Don't make solsym fixed size, but rather of size gas_pcnst
    Signed-off-by: Thibaud Fritz <fritzt@mit.edu>

--- src/chemistry/geoschem/mo_tracname.F90
+++ src/chemistry/geoschem/mo_tracname.F90
@@ -5,13 +5,10 @@
 !           surface fluxes for the advected species.
 !-----------------------------------------------------------
 
-      use chem_mods, only : nTracersMax
+      use chem_mods, only : gas_pcnst
 
       implicit none
 
-! modified to an arbitrary high #, was gas_pcnst. this would cause a memory
-! overflow overwrite in mo_sim_dat, which allocates :273 larger than
-! the default specified gas_pcnst (hplin, 5/16/20)
-      character(len=16) :: solsym(318)   ! species names
+      character(len=16) :: solsym(gas_pcnst)   ! species names
 
       end module mo_tracname
