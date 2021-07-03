79df8eb93b61832d94f3c3175f45411924a9b649
Jesse Nusbaumer <nusbaume@ucar.edu>
2021-07-02 20:59:13 -0600

    Add ChangeLog check and email feature to tag readme update script. Also modified branch push and tag push workflows to only execute on the ESCOMP/CAM repo.

--- .github/scripts/tag_check_readme_update.py
+++ .github/scripts/tag_check_readme_update.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 """
-Script name:  readme_tag_update.py
+Script name:  tag_check_readme_update.py
 
-Goal:  To determine if a recent push to the "development" branch
-       was for a tag, and if so, to update the README.md file on
-       the default branch to display the new development tag.
+Goal:  To determine if the pushed tag was configured properly.
+       If not then the action will "fail", notifying the admins.
 
+       Also, if the pushed tag was configured correctly and was
+       for the "development" branch, then this script will update
+       the README.md file on the default branch to display the new
+       development tag.
 
 Written by:  Jesse Nusbaumer <nusbaume@ucar.edu> - February, 2020
 """
@@ -18,6 +21,9 @@ Written by:  Jesse Nusbaumer <nusbaume@ucar.edu> - February, 2020
 import re
 import sys
 import argparse
+import base64
+import smtplib
+import ssl
 
 from github import Github
 
@@ -46,9 +52,41 @@ def parse_arguments():
     parser.add_argument('--trigger_sha', metavar='<GITHUB SHA>', action='store', type=str,
                         help="Commit SHA that triggered the workflow")
 
+    parser.add_argument('--cam_email_pword', metavar='<CAM PASSWORD>', action='store', type=str,
+                        help="Password for cam.engineering.works email")
+
     #Parse Argument inputs
     args = parser.parse_args()
     return args
+
+#++++++++++++++++++++++
+#Email message function
+#++++++++++++++++++++++
+
+def email_msg(receiver_list, pword, title, msg):
+
+    """
+    Send email message to receiver list via
+    the "cam.engineering.works@gmail.com" email.
+    """
+
+    #Combine email title and message,
+    #so that email is properly formatted:
+    msg_email = "Subject: "+title+"\n\n"+msg
+
+    #create email encryption:
+    #Note that gmail automatically converts SSL to TLS
+    context = ssl.create_default_context()
+
+    #Open email server (gmail always uses port 465):
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        #Login to cam.engineering.works:
+        server.login("cam.engineering.works@gmail.com", pword)
+        #Loop over receiver emails:
+        for receiver in receiver_list:
+            #Send email
+            server.sendmail("cam.engineering.works@gmail.com", receiver,
+                            msg_email)
 
 #++++++++++++++++++++++++++++++++
 #Script message and exit function
@@ -63,11 +101,37 @@ def end_script(msg):
     print("README tag update script has completed successfully.")
     sys.exit(0)
 
+#+++++++++++++++++++++++++++++++++++++++++++
+#Script message and exit function with error
+#+++++++++++++++++++++++++++++++++++++++++++
+
+def end_script_fail(cam_pword, msg):
+
+    """
+    Prints message to screen, and then raises an exception
+    in order to generate a failure notification in Github.
+
+    This function will also send an email to the CAM SEs
+    notifying them that something has gone wrong.
+    """
+
+    receiver_list = ["nusbaume@ucar.edu", "cacraig@ucar.edu", "goldy@ucar.edu", "courtneyp@ucar.edu"]
+    email_msg(receiver_list, cam_pword, "Tag README update failure!", msg)
+
+    print("\n{}\n".format(msg))
+    print("README tag update script was un-successful.")
+    sys.exit(1)
+
+
 #############
 #MAIN PROGRAM
 #############
 
 def _main_prog():
+
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-statements
 
     #++++++++++++
     #Begin script
@@ -84,6 +148,7 @@ def _main_prog():
     #Add argument values to variables:
     token = args.access_token
     trigger_sha = args.trigger_sha
+    cam_pword = args.cam_email_pword
 
     #++++++++++++++++++++++++++++++++
     #Log-in to github API using token
@@ -162,14 +227,14 @@ def _main_prog():
             pr_num = int(first_word[1:]) #ignore "#" symbol
         except ValueError:
             #If the conversion fails, then this is likely not a real PR merge, so end the script:
-            endmsg = "No Pull Request number was found in the tagged commit message. "
-            endmsg += "This is likely a special commit, so the script will not modify README."
-            end_script(endmsg)
+            endmsg = "No Pull Request number was found in the tagged commit message.\n"
+            endmsg += "This is a non-normal tag commit, and so would be worth double-checking."
+            end_script_fail(cam_pword, endmsg)
 
     else:
-        endmsg = "No Pull Request merges were found in the tagged commit message. "
-        endmsg += "This is likely a special commit, so the script will not modify README."
-        end_script(endmsg)
+        endmsg = "No Pull Request merges were found in the tagged commit message.\n"
+        endmsg += "This is likely a non-normal tag commit, and so would be worth double-checking."
+        end_script_fail(cam_pword, endmsg)
 
     #+++++++++++++++++++++++++++++++++++++
     #Check if tagged PR was in fact merged
@@ -180,8 +245,9 @@ def _main_prog():
 
     #If pull request has not been merged, then exit script:
     if not merged_pull.merged:
-        endmsg = "Pull request in commit message was not actually merged, so the script will not update anything."
-        end_script(endmsg)
+        endmsg = "Pull request in tagged commit message was not actually merged.\n"
+        endmsg += "This is likely a non-normal tag commit, and so would be worth double-checking."
+        end_script_fail(cam_pword, endmsg)
 
     #++++++++++++++++++++++++++++++++++++++++++
     #Check if PR was for the development branch
@@ -197,9 +263,67 @@ def _main_prog():
         endmsg = "Tagged PR merged into non-development branch. No further action will thus be taken."
         end_script(endmsg)
 
-    #++++++++++++++++++++++++++++++++
-    #Extrac README file contents/text
-    #++++++++++++++++++++++++++++++++
+    #++++++++++++++++++++++
+    #Extract ChangeLog file
+    #++++++++++++++++++++++
+
+    #Extract contents of "doc" directory:
+    doc_files = cam_repo.get_dir_contents("doc", ref=merged_branch)
+
+    #Extract SHA for ChangeLog:
+    changelog_sha = None
+    for doc_file in doc_files:
+        if doc_file.name == "ChangeLog":
+            changelog_sha = doc_file.sha
+            break
+
+    #If sha can't be found, then it means the ChangeLog is missing,
+    #so throw an error:
+    if not changelog_sha:
+        endmsg = "ChangeLog SHA can't be found, which means no doc/ChangeLog file exists.\n"
+        endmsg += "Please create a new PR ASAP that adds the ChangeLog back to the development branch."
+        end_script_fail(cam_pword, endmsg)
+
+    #The ChangeLog is too large to download using the standard API,
+    #so we need to use the Github data API:
+    changelog_git_blob = cam_repo.get_git_blob(changelog_sha)
+
+    #Convert changelog blob to unicode string:
+    changelog = base64.b64decode(changelog_git_blob.content).decode("UTF-8")
+
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #Search for tag in ChangeLog, to ensure log was properly updated
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    #Make ChangeLog all lowercase, so that searching is case independent:
+    changelog = changelog.lower()
+
+    #Find first occurence of "Tag name" string:
+    tag_log_idx = changelog.find("tag name")
+
+    if tag_log_idx == -1:
+        endmsg = "No 'tag name' entry found in the ChangeLog!\n"
+        endmsg += "The ChangeLog file has likely been corrupted."
+        end_script_fail(cam_pword, endmsg)
+    else:
+        #Find first occurence of "Originator", to set tag search end:
+        orig_log_idx = changelog.find("originator")
+        if orig_log_idx == -1:
+            endmsg = "No 'Originator' entry found in the ChangeLog!\n"
+            endmsg += "The ChangeLog file has likely been corrupted."
+            end_script_fail(cam_pword, endmsg)
+
+    #Search for tag string inbetween "tag name" and "originator":
+    tag_found = changelog[tag_log_idx:orig_log_idx].find(tag_name)
+
+    if tag_found == -1:
+        endmsg = "Newly-created tag '{}' not the first tag listed at top of ChangeLog!\n"
+        endmsg += "Please update the ChangeLog so that the newest tag is listed first."
+        end_script_fail(cam_pword, endmsg.format(tag_name))
+
+    #+++++++++++++++++++++++++++++++++
+    #Extract README file contents/text
+    #+++++++++++++++++++++++++++++++++
 
     #Grab README file object:
     #Note: This command always uses the default branch unless
@@ -244,7 +368,15 @@ def _main_prog():
     #End script
     #++++++++++
 
-    print("cam_development tag has been successfully updated in README file.")
+    endmsg = "cam_development tag has been successfully updated in README file."
+
+    #Google will turn off "non-approved" SMTP access for a gmail account
+    #if not used regularly.  So send an email every time this action is
+    #triggered and updates the README file to ensure that google keeps the access open:
+    email_msg(["nusbaume@ucar.edu"], cam_pword, "Tag README update success!", endmsg)
+
+    #Print end message to Github Action stdout:
+    print(endmsg)
 
 #############################################
 
