c2ec6dcb8b3abeeaacbce59bce85bd8b68fceef8
John Truesdale <jet@ucar.edu>
2021-09-10 12:30:40 -0600

    change dry pressure value to real (add decimal) for _r8 declaration

--- src/utils/physconst.F90
+++ src/utils/physconst.F90
@@ -96,7 +96,7 @@ real(r8), public, parameter :: mwnh4       =  18._r8
 
 ! CESM reference dry mass pressure with/without topo (Pa)
 real(r8), public, parameter :: ps_dry_topo   = 98288.0_r8
-real(r8), public, parameter :: ps_dry_notopo = 101080_r8 !101325 - 245.
+real(r8), public, parameter :: ps_dry_notopo = 101080.0_r8 !101325 - 245.
 
 
 ! modifiable physical constants for aquaplanet
