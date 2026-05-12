b4338bb2dafe092142d47114db57ec9852231c5f
PeterHjortLauritzen <pel@ucar.edu>
2026-05-12 11:42:30 -0600

    fix energy bug in physics-dynamics coupling

--- src/utils/air_composition.F90
+++ src/utils/air_composition.F90
@@ -107,7 +107,7 @@ module air_composition
    real(r8), public, protected, allocatable :: mbarv(:,:,:)
    ! cp_or_cv_dycore:  enthalpy or internal energy scaling factor for 
    !                   energy consistency
-   real(r8), public, protected, allocatable :: cp_or_cv_dycore(:,:,:)
+   real(r8), public,            allocatable :: cp_or_cv_dycore(:,:,:)
    !
    ! Interfaces for public routines
    interface get_cp_dry
