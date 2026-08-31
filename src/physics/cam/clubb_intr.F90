7efb25c13eae908a1010fc31b3445021918ac7a1
adamrher <adamrhster@gmail.com>
2026-08-30 18:33:38 -0600

    addressed some of co-pilot review

--- src/physics/cam/clubb_intr.F90
+++ src/physics/cam/clubb_intr.F90
@@ -4700,7 +4700,7 @@ end subroutine clubb_init_cnst
       do k = 1, nzm_clubb
         do i = 1, ncol
           k_cam = top_lev - 1 + k
-          wp2_pbuf(i,k_cam) = wp2_pbuf(i,k_cam) + s_aww(i,k)
+          wp2_pbuf(i,k) = wp2_pbuf(i,k) + s_aww(i,k)
           khzm_pbuf(i,k_cam) = khzm(i,k) + &
                clubb_params(i,ic_K) * Lscale_zm(i,k) * sqrt(0.5_r8 * s_aww(i,k))
         end do
