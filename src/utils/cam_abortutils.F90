f770c00e30693653ac21aa81edea73153e72fab1
John Alex <jpalex@johns-air.mynetworksettings.com>
2026-09-05 04:42:21 -0400

    Update handle_allocate_error to include optional errmsg (fixes #1322)

--- src/utils/cam_abortutils.F90
+++ src/utils/cam_abortutils.F90
@@ -12,19 +12,25 @@ module cam_abortutils
 
 CONTAINS
 
-   subroutine handle_allocate_error(retval, subname, fieldname)
+   subroutine handle_allocate_error(retval, subname, fieldname, errmsg)
       ! if <retval> is not zero, generate an error message and abort
       ! Dummy arguments
       integer,          intent(in) :: retval
       character(len=*), intent(in) :: subname
       character(len=*), intent(in) :: fieldname
+      character(len=*), optional, intent(in) :: errmsg
+
       ! Local variable
-      character(len=SHR_KIND_CL)   :: errmsg
+      character(len=SHR_KIND_CL)   :: abort_msg
 
       if (retval /= 0) then
-         write(errmsg, '(4a,i0)') trim(subname), ' error allocating ',        &
+         write(abort_msg, '(4a,i0)') trim(subname), ' error allocating ',      &
               trim(fieldname), ', error = ', retval
-         call endrun(errmsg)
+         if (present(errmsg)) then
+            abort_msg = trim(abort_msg) // new_line('a') // &
+                        "Allocation failed with: " // trim(errmsg)
+         end if
+         call endrun(abort_msg)
       end if
    end subroutine handle_allocate_error
 
