b8d15412bcaaf2d730c0175fee2ee253b5d0c0b6
John Truesdale <john@thetruesdales.com>
2026-09-24 20:39:43 -0600

    add call to get_opt for deep_scheme.

--- src/physics/cam/gw_drag_cam.F90
+++ src/physics/cam/gw_drag_cam.F90
@@ -474,6 +474,7 @@ subroutine gw_drag_cam_init()
   character(len=cl)              :: gw_drag_file_loc
   character(len=cl)              :: gw_drag_file_mm_loc
   character(len=cl)              :: gw_drag_file_sh_loc
+  character(len=16)              :: deep_scheme
 
   !-----------------------------------------------------------------------
 
@@ -550,6 +551,8 @@ subroutine gw_drag_cam_init()
   if (use_gw_oro .or. use_gw_rdg_beta .or. use_gw_rdg_gamma) then
      sgh_idx = pbuf_get_index('SGH')
   endif
+
+  call phys_getopts(deep_scheme_out = deep_scheme)
 
   if (deep_scheme == 'ZM' .and. (use_gw_convect_dp .or. use_gw_movmtn_pbl)) then
      ttend_dp_idx = pbuf_get_index('TTEND_DP',errflg)
